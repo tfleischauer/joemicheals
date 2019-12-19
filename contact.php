2a2085632f27912d0ed5e8ab99daae23d1fbebf4
tfleischauer <tfleischauer@gmail.com>
2019-12-18 19:10:54 -0800

    New Contact Form

--- contact.php
+++ contact.php
@@ -1,87 +1,69 @@
 <?php include('includes/header.php'); ?>
                                 
-          <!--<div class="bodyCopy">-->
           	<div id="middle-contact">
          
                 <div id="contact-joe">
                   <h2><em>Contact: Joe Micheals</em></h2>
                   
-                   <address>
-                      <p> Call or Fax Joe at [206] 284.5482 </p><!--, <a href="http://facebook.com/joe.micheals">Facebook</a>-->
-                   </address>
+                  <address>
+                    <p> Call or fax Joe at [206] 284.5482 </p><!--, <a href="http://facebook.com/joe.micheals">Facebook</a>-->
+                  </address>
                     
-                   <form action="http://www.newmanic.com/formhandler3.asp" method="post">
-                        <table>
-                            <tr>
-                                <td>Your Name:</td>
-                                <td><input class="form-field" name="FromName" type="text" id="FromName" size="25" /></td>
-                            </tr>
-                            <tr>
-                                <td>Your Email:</td>
-                                <td><input class="form-field" name="FromEmail" type="text" id="FromEmail" size="25" /></td> <!-- size="17" -->
-                            </tr>
-                            <tr>
-                                <td>Comments:</td>
-                                <td><textarea name="Comments" rows="6" cols="22" spellcheck="true" ></textarea></td><!--Insert your message here, please. size="35"-->
-                            </tr>  
-                            <tr>
-                                <td colspan="2">
-                                    <input type="hidden" name="Subject" value="joemicheals.com" />   
-                                    <input type="hidden" name="ToEmail" value="joemicheals@comcast.net" />
-                                    <input type="hidden" name="ToFirst" value="Joe" />
-                                    <input type="hidden" name="ToLast" value="Micheals" />
-                                    <input type="hidden" name="Thanks" value="http://www.joemicheals.com/thankyou.php" />
-                                    
-                                    <input type="submit" value="Send"   />
-                                 </td>
-                             </tr>
-                        </table>
+                  <h2 class="section-label max940px">Contact</h2>          
+                
+                  <fieldset>
+                    <!--<legend><h2>Contact</h2></legend>-->
+                    <form id="contact-form" action="submit.php" method="post">
+                     <div class="error">
+                      <p>
+                        <label>First Name: </label>
+                        <input type="text" id="first-name" name="first-name" required min-length="2" title="Please enter your first name." />
+                      </p>
+                      
+                      <p>
+                        <label>Last Name: </label>
+                         <input type="text" id="last-name" name="last-name" required min-length="2" title="Please enter your last name." />
+                      </p>
+                      
+                      <p>
+                        <label for="email">Email:</label>
+                        <input type="email" id="email" name="email" required title="Please enter a valid email address." /> <!--required data-msg-email-->
+                      </p>
+                      
+                      <p>
+                        <label for="subject">Subject:</label>
+                        <input type="text" id="subject" name="subject" required title="What is this email regarding?">
+                      </p>
+                      
+                      <p>
+                        <label for="message">Message:</label>
+                        <textarea name="message" id="message" required title="Please enter your message."></textarea>
+                      </p>
+                      
+                      <p id="buttons">
+                        <button type="submit" name="submit" value="submit">Send Message</button>
+                      </p>
+                      
+                     </div> <!-- end error div -->
                     </form>
-                    
-                    <!-- <form action="/stagingarea/joemicheals/" method="post"> http://www.example.com/upload.php Duckett p.159
-                    <p>Upload your script: </p>
-                    <input type="file" name="user-doc" /><br />
-                    <input type="submit" value="Upload" />
-                    </form> -->
-              
-                   </div> <!-- end contact-joe -->
-                   
-                   <div class="cashman-drawing">
-                    	<img src="images/joeByCashman-back-500x651px-3.jpg" alt="Illustration of Joe Micheals by Pat Cashman" title="Illustration by Pat Cashman">
-                   </div>
+                  </fieldset>
+                       
+                  <div id="result"></div>
                  
-       		  <!--</div>--> <!-- end bodyCopy -->
-              </div> <!-- end middle-contact --> 
+                </div> <!-- end contact-joe -->
+                  
+                <div class="cashman-drawing">
+                      <img src="images/joeByCashman-back-500x651px-3.jpg" alt="Illustration of Joe Micheals by Pat Cashman" title="Illustration by Pat Cashman">
+                </div>
+
+			  </div> <!-- end middle-contact --> 
               
+              <script src="js/jquery-2.1.1.min.js"></script>
+    
+    		  <!-- form is for the ajax submit -->
+              <script src="js/jquery.form.js"></script> 
+              <script src="js/jquery.validate.min.js"></script>
+              <script src="js/formscript.js"></script> 
+
 <?php include("includes/footer.php"); ?>
-
-         <!--<footer>
-         	<p>&copy; 2014 Joe Micheals. All rights reserved.</p>
-            <p>Illustration by Pat Cashman</p>
-         </footer>-->
-         
-        
-         
-         <!--<p id="validation-icons">
-            <a href="http://validator.w3.org/check?uri=referer">
-            <img src="http://www.w3.org/html/logo/badge/html5-badge-h-solo.png" 
-            width="31" height="32" alt="valid HTML5" />
-            </a>
-            <a href="http://jigsaw.w3.org/css-validator/check/referer?profile=css3">
-            <img src="http://jigsaw.w3.org/css-validator/images/vcss" 
-             alt="Valid CSS3" height="31" width="88" />
-            </a>
-         </p>-->
-        <!-- end validation buttons -->
-        
-         <!--</div>--> <!-- end wrapper -->
-
-<!--      </body>
-</html>-->
-              
-             
-              
-       <!--  </div>--> <!-- end bodyCopy -->
-         
-
             
