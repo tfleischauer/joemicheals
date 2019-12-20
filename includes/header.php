bb104e35740a03a01ef83f31f0e6d248c9f60928
tfleischauer <tfleischauer@gmail.com>
2019-12-20 14:31:06 -0800

    Update Copy and Indentation

--- includes/header.php
+++ includes/header.php
@@ -2,7 +2,7 @@
 
 <!-- 
 
-Joe Micheals: Voice Over the Northwest
+Joe Micheals: Voice-Over the Northwest
 
 Web Design 2012 by Troy Fleischauer
 http://portfolio.troyfleischauer.com
@@ -14,7 +14,7 @@ http://portfolio.troyfleischauer.com
 		<title>Joe Micheals | <?php include('includes/titles.php'); ?></title>
         <meta charset="UTF-8">
         <meta name="ROBOTS" content="INDEX, FOLLOW">
-        <meta name="description" content="Joe Micheals: Voice Over the Northwest. Voice-over talent and air personality." />
+        <meta name="description" content="Joe Micheals: Voice-Over the Northwest. Voice-over talent and air personality." />
         <meta name=viewport content="width=device-width, initial-scale=1"> 
         
         <!-- Remy Sharp Shim --> 
@@ -23,19 +23,18 @@ http://portfolio.troyfleischauer.com
 		<![endif]-->
     
 		<link rel="stylesheet" type="text/css" href="jmstylesheet.css" />
-        <link href='http://fonts.googleapis.com/css?family=PT+Sans:400,400italic,700,700italic' rel='stylesheet' type='text/css'>
-             
+        <link href='http://fonts.googleapis.com/css?family=PT+Sans:400,400italic,700,700italic' rel='stylesheet' type='text/css'>            
     </head>
     
     <body>
-    	<div id="wrapper">
+        <div id="wrapper">
     
-      <header>
-      		<h1><a href="http://www.joemicheals.com/"> <img src="images/JMSignatureTrace2-405x160px-transparent.png" alt="Joe Micheals header image" /></a><span>Joe Micheals: Voice-Over Artist</span></h1>
-            <h2 class="tagline"><a href="http://www.joemicheals.com/">Joe Micheals | Voice Over The Northwest</a></h2>
-  	  </header>  <!-- End Header -->
-
-        <?php include ('includes/navigation.php'); ?>
-		
-        <!-- Begin Middle -->
+            <header>
+                  <h1><a href="http://www.joemicheals.com/"> <img src="images/JMSignatureTrace2-405x160px-transparent.png" alt="Joe Micheals header image" /></a><span>Joe Micheals: Voice-Over Artist</span></h1>
+                  <h2 class="tagline"><a href="http://www.joemicheals.com/">Joe Micheals | Voice-Over The Northwest</a></h2>
+            </header>  <!-- End Header -->
+      
+            <?php include ('includes/navigation.php'); ?>
+              
+            <!-- Begin Middle -->
               
