ff6c1e786983c6daeea1c3e3b03e6b275fc95871
tfleischauer <tfleischauer@gmail.com>
2019-12-31 19:13:21 -0800

    add Portal 2 page

---
+++ portal2.php
@@ -0,0 +1,88 @@
+<?php include('includes/header.php'); ?>
+
+<div class="middle">
+
+<h2>Portal 2 (Valve)</h2>
+
+<section>
+  
+      <h3>Announcer</h3>
+      
+      <div class="text-left">
+                
+            <p>The recording sessions for <em>Portal 2</em> were great fun.  1 bit of direction was to <q cite="http://www.thinkwithportals.com/">make it sound like you were the announcer for a ride at Disneyland!</q></p>  
+            
+            <p>I really didn't know how the final voice would sound. The effect was quite surprising!  So are some of the <a href="http://www.youtube.com/watch?v=TE67EpU9u40" target="_blank">comments</a> on the blogsites.</p>
+            
+            <p>Surprisingly, there were a few fans who compiled the <abbr title="voice-overs">V/Os</abbr> of Announcer for Portal 2 and posted them to YouTube.</p>
+            
+      <h4>My Announcer Session</h4>
+            
+            <p>Valve News Network produced a couple videos that show the process of how my Announcer voice tracks were directed.</p>
+                                   
+            <p class="list-description">More Portal 2:</p>
+            <ul>
+                <li><a href="http://www.imdb.com/title/tt1648163/?ref_=nm_ov_bio_lk1" target="_blank">IMDb - Portal 2</a></li>
+                <li><a href="http://theportalwiki.com/wiki/Announcer" target="_blank">Announcer - Portal Wiki</a></li>
+                <li><a href="http://www.thinkwithportals.com/" target="_blank">Official Portal 2 Website</a></li>
+            </ul>
+            
+       </div> <!-- end text-left -->
+       
+       <div class="video-div">
+       
+            <div class="iframe-wrapper">
+              <iframe class="iframe-embed" src="//www.youtube.com/embed/S1YzIMpDBbQ" allowfullscreen></iframe>
+            </div> <!-- end iframe wrapper --> 
+                 
+         <p><small>Compilation of 'Announcer' voice-overs that I recorded for Portal 2</small></p>
+         
+       </div> <!-- end video-div -->
+       
+       <div class="video-div">
+       
+            <div class="iframe-wrapper">
+              <iframe class="iframe-embed" src="https://www.youtube.com/embed/3uzvySs6yHo?ecver=2" allowfullscreen></iframe>
+            </div> <!-- end iframe wrapper --> 
+                 
+         <p><small>Unheard Portal Voice Files - How Valve Directs Voice Actors (with Joe Micheals)</small></p>
+         
+       </div> <!-- end video-div -->
+       
+       <div class="video-div">
+       
+            <div class="iframe-wrapper">
+              <iframe class="iframe-embed" src="https://www.youtube.com/embed/h7mu4JN_FOA?ecver=2" allowfullscreen></iframe>
+            </div> <!-- end iframe wrapper --> 
+                 
+         <p><small>More Beta Portal Voice Files - How Valve Directs Voice Actors (with Joe Micheals)</small></p>
+         
+       </div> <!-- end video-div -->
+</section>
+
+<section>
+  
+      <h3>Anti-Hero</h3>
+      <h4>Voice of The City</h4>
+      
+      <div class="text-left">
+                
+            <p>Another voice actor role that I do is Voice of The City for this online original comic series.</p>
+            
+       </div> <!-- end text-left -->
+       
+       
+       <div class="video-div">
+       
+            <div class="iframe-wrapper">
+              <iframe class="iframe-embed" src="https://www.youtube.com/embed/qmmouuBlWTk" allowfullscreen></iframe>
+            </div> <!-- end iframe wrapper --> 
+                 
+         <p><small>Anti-Hero Episode 1: Origin Story</small></p>
+         
+       </div> <!-- end video-div -->
+</section>
+
+</div> <!-- end middle -->
+        
+<?php include("includes/footer.php");   ?>
