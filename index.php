558f4bf266d821fabba2e643b1df438ac8c3aec2
tfleischauer <tfleischauer@gmail.com>
2019-12-31 19:31:06 -0800

    add King 5 video

--- index.php
+++ index.php
@@ -1,16 +1,20 @@
 <?php include('includes/header.php'); ?>
                                 
           <div class="middle">
-          	<h2><em>Voice-Over Home</em></h2>
+          	<h2>Voice-Over Home</h2>
             
-           <div class="area460px">
-
-              <p>I am a freelance voice-over artist in Seattle. If you would like an audition for your project please <a href="mailto:joemicheals@comcast.net">e-mail</a> your script to joemicheals at comcast.net. I will return a voice sample at my earliest opportunity. Or reach me by phone or fax at [206] 284.5482.</p><!--<a href="mail.html" onmouseover="this.href='mai' + 'lto:' + 'joemicheals' +  '@' + 'comcast.net'">-->
+           <div class="text-left">
+           
+			  <h3>Welcome</h3>
+              
+              <p>I am a freelance voice-over artist in Seattle.</p> 
+              <p>If you would like an audition for your project please <a href="mailto:joemicheals@comcast.net">e-mail</a> your script to joemicheals at comcast.net.</p> 
+              <p>I will return a voice sample at my earliest opportunity. Or reach me by phone or fax at [206] 284.5482.</p><!--<a href="mail.html" onmouseover="this.href='mai' + 'lto:' + 'joemicheals' +  '@' + 'comcast.net'">-->
 
             </div> <!-- end area460px -->
               
-            <div class="area310px">
-            	 <h3>Voice-Over Demos</h3>
+            <div class="audio-video-div">
+            	 <h3>Demos</h3>
                  
                  <div class="audio-player audio-player-margin-top">
                                     
@@ -35,7 +39,18 @@
                          <p class="audio-caption">Two Minute Demo</p>
                         
                  </div> <!-- end audio-player -->
-            </div> <!-- end area310px -->           
+            
+                <div class="video-div">     
+        
+                      <div class="iframe-wrapper">
+                        <iframe class="iframe-embed" src="https://www.youtube.com/embed/dAwKj6Ryppw" allowfullscreen></iframe> <!-- frameborder="0" -->
+                      </div> <!-- end iframe wrapper --> 
+                           
+                      <!--<p class="audio-caption">Here is a sample of my King 5 voice-overs. I was the announcer at King 5 from 1981 to 2017.</p>-->
+               
+                </div>  
+                
+            </div> <!-- end audio-video-div -->         
               
          </div> <!-- end middle-home -->
          
