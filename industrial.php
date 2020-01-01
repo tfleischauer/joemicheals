58fe7ba2b1405976c860e8ad715213f5bdad92bc
tfleischauer <tfleischauer@gmail.com>
2019-12-31 19:30:33 -0800

    add Industrial page

---
+++ industrial.php
@@ -0,0 +1,72 @@
+<?php include('includes/header.php'); ?>
+
+<div class="middle">
+
+	<h2>Industrial Voice-Over</h2>
+    
+        <div class="area460px">
+                                 
+           <h3>Internal Training</h3> 
+           	<p>I’ve narrated internal instructional and training videos for a variety of clients such as:</p>      
+              <ul>
+                  <li>Boeing</li>
+                  <li>Genie</li>
+                  <li>McCaw Cellular Communications</li>
+                  <li>Microsoft</li>
+                  <li>PACCAR</li>
+                  <li>Tel-Med</li>
+                  <li>Weyerhaeuser</li>
+                  <li>World Vision</li>
+              </ul>
+              
+         </div>
+         
+         
+         <div class="area310px">
+                    
+             <!--<h3>Audio</h3>           
+	                
+              <div class="audio-player-310 audio-player-margin-top">
+                  
+                  <audio controls>  
+                    <source src="audio/JoeMichealsSpirit10532010Promos.mp3" type="audio/mpeg">
+                    <source src="audio/JoeMichealsSpirit10532010Promos.ogg" type="audio/ogg">
+                    Your browser does not support the audio element. Download <a href="../audio/JoeMichealsSpirit10532010Promos.mp3">Weyerhaeuser Demo</a>
+                  </audio> 
+                  <p class="audio-caption">Weyerhaeuser</p>
+                    
+              </div>--> <!-- end audio-player -->
+              
+             <!-- <div class="border-bottom"></div>-->
+              
+          </div> <!-- area310px -->  
+          
+          <section>         
+       
+      <h3 class="add-margin-top">External Training</h3>
+      <h4 class="add-margin-top">Personal Trainer: Cooking - DS Chef (Nintendo)</h4>
+       
+      <div class="area460px">
+                
+          <p><em>Personal Trainer: Cooking</em> was produced for <a href="http://www.nintendo.com/ds" target="_blank">Nintendo DS</a>, and as the name implies, it is interactive instructional software for preparing recipes that is accessible through a portable device.</p>
+          <p>I did the instructional voice work to guide the user through preparing each dish.</p>
+          
+          <!--<p class="list-description">More Personal Trainer: Cooking</p>
+          
+          <ul>
+                <li><a href="http://personaltrainercooking.com/features/index.html#features-ds-chef" target="_blank">Official Site</a></li>
+                <li><a href="http://www.nintendo.com/games/detail/0GH0z7XuqgMVHravefY0EVUR_xzNIXa7" target="_blank">Nintendo Games</a></li>
+          </ul>-->
+            
+      </div> <!-- end text-left -->
+       
+      <div class="area310px">
+         <a id="nintendoImage" href="http://personaltrainercooking.com" target="_blank"> <img src="images/Personal_Trainer_Cooking_310x281px-white.png" alt="Personal Trainer: Cooking (Nintendo)" /></a>       
+          <!--<p><small>Personal Trainer: Cooking (Nintendo)</small></p>-->       
+      </div> <!-- end video-div -->
+                
+	</section>
+         
+</div> <!-- end middle -->
+        
+<?php include("includes/footer.php");   ?>
