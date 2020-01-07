e1df9adbcdd797af91203cd0d5156dfb64bccc2d
tfleischauer <tfleischauer@gmail.com>
2020-01-07 13:32:38 -0800

    new Bio layout

--- bio.php
+++ bio.php
@@ -1,51 +1,64 @@
 <?php include('includes/header.php'); ?> 
                       
 <div class="bodyCopy">
-  <!--<h1>Bio</h1>-->
+
   <h2>About the Voice-Over Guy</h2>
   
-  <div id="BroadcastProgramming">
-  
-    <p>
-       <img class="hoveringicon" src="images/JMBroadcastProgramming238x283-3pxBorder.png" alt="Joe Micheals with Program Broadcasting (1980)" />
-       
-      I was always enchanted by the way that words and pictures could fly through the air. Listening to radio and eventually doing voice-overs for radio and television in San Francisco and Seattle was beyond my wildest imagination.</p>
-      
-      <p>The radio was always a fascinating thing to me. At an early age I knew I wanted to be a broadcaster. When I was a teenager, I would put loud speakers outside my basement window in Millwood, and broadcast to the neighborhood. I would create commercials by reading ads out of the newspaper and mix that in with Top 40 music.</p>
-      
-      <p>Eventually J. Shannon Sweatte, manager at KJRB in Spokane, who was picnicking in the park across the street from where I lived heard my broadcast and through a series of “coincidences” hired me at the local radio station, KJRB.</p>
-      
-      <p>KJRB was owned by actor Danny Kay and businessman Lester Smith, they also owned other radio stations in different markets, and through them I was able to work in Seattle which lead to a subsequent job in San Francisco.</p> 
+  <div id="Bio-1">
     
-  </div> <!-- end BroadcastProgramming -->
-  
-  
-  <div id="Joe1">
-    <p>
-      <img class="hoveringRIGHTimage" src="images/joemicheals-163x236.png" alt="Joe Micheals (2004)" />
-    
-      Part of the job description of the radio and television business was doing commercials. I grew to appreciate the artistry of doing voice for various audio and film projects. Doing <abbr title="voice-overs">V/O's</abbr> exclusively was a natural evolution.</p> 
+      <img class="four-of-twelve-columns-left Joe-Left" src="images/JMBroadcastProgramming-260x310px.jpg" alt="Joe Micheals with Program Broadcasting (1980)" />
       
-    <p>Since doing V/O’s full time, I‘ve had the privilege of working with some of America’s finest companies including Microsoft, Weyerhauser, Nordstrom, Valve, Safeway, Boeing, and Nintendo. These are a few that are still in business.</p> <!--See resume of companies still in business.-->
-    
-    <p>I’ve always enjoyed inflating the benefits of products and services to the maximum boundary of truth. I’d like to walk the fine line between truth and fiction about your product or service.</p>   
+      <div class="eight-of-twelve-columns-right"> 
       
- </div> <!-- end Joe1 -->
- 
- 
- <div id="SeattleTimes">
- 
-    <p> 
-        <img class="hoveringicon" src="images/JMSeattleTimes175x195.png" alt="Joe Micheals on Seattle Times News Box" />
+        <h3>Millwood, WA</h3> 
         
-        <em>Have you ever noticed these newspaper boxes around Western Washington?</em></p> 
+        <p>I was always enchanted by the way that words and pictures could fly through the air. Listening to radio and eventually doing voice-overs for radio and television in San Francisco and Seattle was beyond my wildest imagination.</p>
         
-        <p>In 1992, Joe was one of the media models who was photographed reading a Seattle Times newspaper. Over twenty years later, these mailboxes can still be found all over Western Washington.</p>
+        <p>The radio was always a fascinating thing to me. At an early age I knew I wanted to be a broadcaster. When I was a teenager, I would put loud speakers outside my basement window in Millwood, and broadcast to the neighborhood. I would create commercials by reading ads out of the newspaper and mix that in with Top 40 music.</p>
         
-    <p> Joe has also won an <a href="http://www.aafseattle.com/addy">Addy Award</a> for King 5 News, and has been known for the special skill of Master of Ceremonies.</p>          
-             
-</div> <!-- end SeattleTimes -->
+        <h3>KJRB > San Francisco</h3> 
+        
+        Eventually J. Shannon Sweatte, manager at KJRB in Spokane, who was picnicking in the park across the street from where I lived heard my broadcast and through a series of “coincidences” hired me at the local radio station, KJRB.</p>
+        
+        <p>KJRB was owned by actor Danny Kay and businessman Lester Smith, they also owned other radio stations in different markets, and through them I was able to work in Seattle which lead to a subsequent job in San Francisco.</p> 
+      
+      </div> <!-- end .eight-of-twelve-columns-right -->
 
+  </div> <!-- end Bio-1 -->
+  
+  
+  <div id="Bio-2">
+      <img class="four-of-twelve-columns-left Joe-Left" src="images/joe-michaels2-097-260x310px.jpg" alt="Joe Micheals (2004)" />
+      
+      <div class="eight-of-twelve-columns-right">  
+    
+    	<h3>Commercials</h3>
+         
+        <p>Part of the job description of the radio and television business was doing commercials. I grew to appreciate the artistry of doing voice for various audio and film projects. Doing <abbr title="voice-overs">V/O's</abbr> exclusively was a natural evolution.</p> 
+        
+        <p>Since doing V/O’s full time, I‘ve had the privilege of working with some of America’s finest companies including Microsoft, Weyerhauser, Nordstrom, Valve, Safeway, Boeing, and Nintendo. These are a few that are still in business.</p> <!--See resume of companies still in business.-->
+        
+        <p>I’ve always enjoyed inflating the benefits of products and services to the maximum boundary of truth. I’d like to walk the fine line between truth and fiction about your product or service.</p>  
+        
+      </div> <!-- end .eight-of-twelve-columns-right -->
+      
+  </div> <!-- end Bio-2 -->
+ 
+  <div id="SeattleTimes">
+ 
+        <img class="four-of-twelve-columns-left Joe-Left" src="images/Joe-Micheals-Seattle-Times-260x310px.jpg" alt="Joe Micheals on Seattle Times News Box" />
+        
+        <div class="eight-of-twelve-columns-right">
+        
+        	<h3><em>Have you ever noticed these newspaper boxes around Western Washington?</em></h3> 
+        
+        	<p>In 1992, Joe was one of the media models who was photographed reading a Seattle Times newspaper. Over twenty-five years later, these mailboxes can still be found all over Western Washington.</p>
+        
+    		<p>Joe has also won an <a href="http://www.aafseattle.com/addy">Addy Award</a> for King 5 News, and has been known for the special skill of Master of Ceremonies.</p>			
+            
+         </div> <!-- end .eight-of-twelve-columns-right -->          
+             
+  </div> <!-- end SeattleTimes -->
 
 </div> <!-- bodyCopy -->             
                
