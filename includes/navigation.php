24b4955dcc228590a0859fa41377d55177f52c3d
tfleischauer <tfleischauer@gmail.com>
2020-01-07 18:19:15 -0800

    indentation

--- includes/navigation.php
+++ includes/navigation.php
@@ -2,8 +2,8 @@
 
             <!-- Begin Navigation"  id="navigation-items-->
             <div class="navicon-container">
-        <span class="nav-button"></span> MENU
-      </div>
+        		<span class="nav-button"></span> MENU
+      		</div>
             <nav> 
                 <ul class="myMenu">
                     <li<?php if ($page == 'index.php' ) { print ' class="main-on"'; } ?>><a href="index.php" class="first">Home</a></li>
